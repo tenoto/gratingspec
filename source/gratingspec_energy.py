63f03241aec839ae5f07aef89e783828a42eb194
Teruaki Enoto <teruaki.enoto@riken.jp>
2023-07-24 17:54:07 +0900

    add

--- source/gratingspec_energy.py
+++ source/gratingspec_energy.py
@@ -20,6 +20,9 @@ def wavelength_nm_to_eV(wavelength_nm):
 def eV_to_wavelength_nm(eV):
 	return eV/1239.8 # nm 
 
+def gaussian(x, mean, std_dev):
+    return np.exp(-((x - mean) ** 2) / (2 * std_dev ** 2)) / (std_dev * np.sqrt(2 * np.pi))
+
 def wavelength_to_rgb(wavelength, gamma=0.8):
 	'''
 	copied from https://gist.github.com/error454/65d7f392e1acd4a782fc
@@ -103,6 +106,40 @@ def grating_spectrum(df_lines,
 	plt.savefig(fname_pdf, format='pdf', facecolor='black')
 	plt.savefig(fname_jpg, format='jpg', facecolor='black')	
 
+ef grating_spectrum_gauss(df_lines,
+	fname_pdf='spectrum_gauss.pdf',fname_jpg='spectrum_gauss.jpg',
+	linewidthfactor=8.0):
+
+	fig, ax = plt.subplots(figsize=(10, 1.5))
+	energy_min = 1.4
+	energy_max = 3.4
+	ax.set_xlim(energy_min, energy_max)
+	ax.set_ylim(0, 1)
+	ax.set_aspect(0.15)
+	plt.subplots_adjust(left=0.01,right=0.99,bottom=0.01,top=0.99)
+	#plt.xlabel('Energy (eV)')
+	#plt.subplots_adjust(left=0.0, right=0.0, bottom=0.0, top=0.0)
+	#ax.set_aspect('auto')
+	ax.set_facecolor('black')
+	ax.spines['bottom'].set_color('white')
+	ax.spines['bottom'].set_linewidth(0.5)
+	ax.tick_params(axis='x', colors='white')
+	ax.xaxis.label.set_color('white')
+	ax.yaxis.set_visible(False)
+
+	for index, row in df_lines.iterrows():
+		wavelength_nm = float(row['wavelength_nm'])
+		energy_eV = wavelength_nm_to_eV(wavelength_nm)
+		intensity = linewidthfactor*np.sqrt(float(row['intens_relative']))
+		print(wavelength_nm,energy_eV,intensity)
+		#intensity = linewidthfactor*np.float(float(row['intens_relative']))
+		color = wavelength_to_rgb(wavelength_nm)
+		#ax.plot([wavelength, wavelength], [0, 1], color=color, linewidth=1.0*intensity)
+		ax.plot([energy_eV, energy_eV], [0, 1], color=color, linewidth=1.0*intensity)
+    
+	plt.savefig(fname_pdf, format='pdf', facecolor='black')
+	plt.savefig(fname_jpg, format='jpg', facecolor='black')	
+
 def read_csv_to_dflines(csv_file):
 	df = pd.read_csv(csv_file)
 
@@ -132,8 +169,12 @@ def read_csv_to_dflines(csv_file):
 
 args = sys.argv
 fname_csv = args[1] # e.g., 'data/Na_I.csv'
+df_lines = read_csv_to_dflines(fname_csv) 
+
 fname_pdf = os.path.splitext(os.path.basename(fname_csv))[0] + '_spec.pdf'
 fname_jpg = os.path.splitext(os.path.basename(fname_csv))[0] + '_spec.jpg'
-df_lines = read_csv_to_dflines(fname_csv) 
 grating_spectrum(df_lines,fname_pdf=fname_pdf,fname_jpg=fname_jpg)
 
+fname_pdf = os.path.splitext(os.path.basename(fname_csv))[0] + '_spec_gauss.pdf'
+fname_jpg = os.path.splitext(os.path.basename(fname_csv))[0] + '_spec_gauss.jpg'
+grating_spectrum_gauss(df_lines,fname_pdf=fname_pdf,fname_jpg=fname_jpg)
